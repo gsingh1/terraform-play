a66ab4d4bbf368034ab382e6252ee97af9e05e3f
Gurarpit Singh <gsingh.ldn@gmail.com>
2019-02-25 22:42:40 +0000

    added subnets

--- resources.tf
+++ resources.tf
@@ -7,3 +7,27 @@ resource "aws_vpc" "env-example" {
     Name = "terraform-aws-vpc"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = "${aws_vpc.env-example.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.env-example.cidr_block, 3, 1)}"
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = "${aws_vpc.env-example.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.env-example.cidr_block, 2, 2)}"
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_security_group" "subnet-security" {
+  vpc_id = "${aws_vpc.env-example.id}"
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.env-example.cidr_block}"
+    ]
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
